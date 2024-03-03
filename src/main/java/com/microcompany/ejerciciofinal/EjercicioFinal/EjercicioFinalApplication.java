22d30a5b5463295690a095766d7a7af5f71f1349
mariadxc <maria-montserrat.martinez-arcas@dxc.com>
2024-03-03 10:54:33 +0100

    spring initializr

---
+++ src/main/java/com/microcompany/ejerciciofinal/EjercicioFinal/EjercicioFinalApplication.java
@@ -0,0 +1,13 @@
+package com.microcompany.ejerciciofinal.EjercicioFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microcompany/ejerciciofinal/EjercicioFinal/EjercicioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcompany.ejerciciofinal.EjercicioFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
